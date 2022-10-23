631ee1e1003705fae2377afa40e4e89e832c5560
BykovaAlyona <80817651+BykovaAlyona@users.noreply.github.com>
2022-10-23 21:34:04 +0500

    Update addnav.js

--- js/addnav.js
+++ js/addnav.js
@@ -1,15 +1,15 @@
 this.nav.innerHTML =
     `<ul style="list-style: none; margin:10px; text-align: center; border-bottom:1px solid grey;">
         <li style="margin: 5px 10px; display: inline-block;">
-            <a href="/test-task-nevatrip/">1.1. Верстка блока</a>
+            <a href="index.html">1.1. Верстка блока</a>
         </li>
         <li style="margin: 5px 10px; display: inline-block;">
-            <a href="/test-task-nevatrip/task-1-2.html">1.2. Таблица на странице</a>
+            <a href="task-1-2.html">1.2. Таблица на странице</a>
         </li>       
         <li style="margin: 5px 10px; display: inline-block;">
-            <a href="/test-task-nevatrip/task-2-1.html">2.1. Билеты на событие</a>
+            <a href="task-2-1.html">2.1. Билеты на событие</a>
         </li>
         <li style="margin: 5px 10px; display: inline-block;">
-            <a href="/test-task-nevatrip/task-2-2.html">2.2. Время из A в B</a>
+            <a href="task-2-2.html">2.2. Время из A в B</a>
         </li>
     </ul>`;
